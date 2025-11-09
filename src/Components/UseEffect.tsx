235a772e44a70c742f2eff683a367b139c701430
Guhirwa <christianguhirwa74@gmail.com>
2025-11-09 14:29:35 +0200

    feat(UseEffect): Using the useEffect I made a simpleapp to change the document title
    - This can be applied in the message tab where you want to show the user the number of unread message at the document title
    - useEffect have two parameter. The first one is a callBack function which determine the effect to make and the second one called array of dependencies determine when to make that effect
    - The array of dependencies accor in three main forms:
    	1. No dependence at all: The effect accour everytime react app re-render
    	2. Empty array: Effect at the first render
    	3. variable present: Effect occur every time variable specified in the array changes.

--- src/Components/UseEffect.tsx
+++ src/Components/UseEffect.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import Button from "./Button";
 
 const UseEffect = () => {
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(1);
+    const [otherCount, setOtherCount] = useState(5)
+
+    useEffect(() => {
+        document.title = `(${count}) new message!`;
+    }, [otherCount, count])
 
   return (
-    <div>
-        <h3>{count} new Messages!</h3>
-        <button onClick={() => setCount(prev => prev + 1)}>Increase</button>
+    <div className="mt-20 flex gap-20 absolute left-1/2 transform -translate-x-1/2">
+        <div className="grid justify-center">
+            <h3 className="font-semibold text-3xl">{count} new Messages!</h3>
+            <Button label="Increase" clickHandler={() => setCount(prev => prev + 1)}/>
+        </div>
+        <div>
+            <h3 className="font-semibold text-3xl">Other Count: {otherCount}</h3>
+            <Button label="Increase by five" clickHandler={() => setOtherCount(prev => prev + 5)}/>
+        </div>
     </div>
   )
 }
